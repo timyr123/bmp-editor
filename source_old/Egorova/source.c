cb021a8e47482d993bd6352c1a65780aa0b2b6ef
astel72 <44057060+astel72@users.noreply.github.com>
2018-10-18 13:31:47 +0300

    Update source.c

--- source_old/Egorova/source.c
+++ source_old/Egorova/source.c
@@ -1,85 +1,108 @@
-#include <stdio.h>
-#include <stdlib.h>
+//kyrsovaya Преобразование изображений средствами языка С
+//sravnit 2 bmp i poschitat kolichestvo raznih tochek
+
+
+#include "stdio.h"
+#include "string.h"
+#define MAX_FILE_SIZE 1000
 
 typedef unsigned char BYTE;
 enum colors {blue, green, red};
-typedef struct{BYTE blue ; BYTE green; BYTE red;} PIXEL;
 
-int main()
+//struct {BYTE blue ; BYTE green; BYTE red;} ;
+
+
+main()
 {
-	int x1, x2, y1, y2;
+    printf("%d %d %d\n", blue , green, red);
+    int i,j,z, chet, dlya_vivoda;
+    char type[2];  
+    int size;
+    int res;
+    int off;
+    int ssz;
+    int w;
+    int h;
+    int end[7];
+    char type2[2];  
+    int size2;
+    int res2;
+    int off2;
+    int ssz2;
+    int w2;
+    int h2;
+    int end2[7];
+    chet = 0;
+    FILE * fin1 = fopen("3.bmp", "r");
+    perror("Open file");
 
-	printf("Enter x1	");
-	scanf("%d", &x1);
-	printf("Enter x2	");
-	scanf("%d", &x2);
-	printf("Enter y1	");
-	scanf("%d", &y1);
-	printf("Enter y2	");
-	scanf("%d", &y2);
+    FILE * fin2 = fopen("4.bmp", "r");
+    perror("Open file");
+    
 
-	x1 = x1 - 1;
-	x2 = x2 + 1;
-	y1 = y1 - 1;
-	y2 = y2 + 1;
-   
-	int i, j, chet, size, res, off, ssz, w, h, h1, h2;
-	char type[2];  
-	int end[7]; 
-	chet = 0;
-
-
-	FILE * fr = fopen("infile.bmp", "r");
-	perror("Open file input");
-
-	fread(&type, 2, 1, fr);
-	fread(&size, 4, 1, fr);
-	fread(&res, 4, 1, fr);
-	fread(&off, 4, 1, fr);
-	fread(&ssz, 4, 1, fr);
-	fread(&w, 4, 1, fr);
-	fread(&h, 4, 1, fr);
-	fread(end, 4, 7, fr);
-
-	PIXEL rastr1[500][500];
-
-	int padding1=(size-off-h*w*3)/h;
-	BYTE pad1[20];   
-
-	for(i=0; i<h ; i++){
-		for(j=0; j<w ; j++){
-			if (((i>x1)&&(i<x2))&&((j<y2)&&(j>y1))){
-				fread(&rastr1[i][j], 3, 1, fr);
-				h1=h1+1;
-      				fread(pad1, padding1, 1, fr);  
-			} 
-   		}
-	h2=h2+1;
-	}
+    fread(&type, 2, 1, fin1);
+    fread(&size, 4, 1, fin1);
+    fread(&res, 4, 1, fin1);
+    fread(&off, 4, 1, fin1);
+    fread(&ssz, 4, 1, fin1);
+    fread(&w, 4, 1, fin1);
+    fread(&h, 4, 1, fin1);
+    fread(end, 4, 7, fin1);
+    fread(&type2, 2, 1, fin2);
+    fread(&size2, 4, 1, fin2);
+    fread(&res2, 4, 1, fin2);
+    fread(&off2, 4, 1, fin2);
+    fread(&ssz2, 4, 1, fin2);
+    fread(&w2, 4, 1, fin2);
+    fread(&h2, 4, 1, fin2);
+    fread(end2, 4, 7, fin2);
 
 
-	w = h1;
-	h = h2;
+     BYTE rastr1[MAX_FILE_SIZE][MAX_FILE_SIZE][3];
+     BYTE rastr2[MAX_FILE_SIZE][MAX_FILE_SIZE][3];
+
+    int padding1=(size-off-h*w*3)/h;
+    BYTE pad1[30];   
+    int padding2=(size2-off2-h2*w2*3)/h2;
+    BYTE pad2[30];
+    
+    for(i=0; i<h ; i++)
+    {
+	for(j=0; j<w ; j++)
+		fread(&rastr1[i][j], 3, 1, fin1);
+        fread(pad1, padding1, 1, fin1);        
+    }	
 
 
-	FILE * fw = fopen("outfile.bmp", "w");
-   	perror("Open file output");
+    for(i=0; i<h2 ; i++)
+    {
+	for(j=0; j<w2 ; j++)
+		fread(&rastr2[i][j], 3, 1, fin2);
+        fread(pad2, padding2, 1, fin2);        
+    }
 
-	fwrite(&type, 2, 1, fw);
-	fwrite(&size, 4, 1, fw);
-	fwrite(&res, 4, 1, fw);
-	fwrite(&off, 4, 1, fw);
-	fwrite(&ssz, 4, 1, fw);
-	fwrite(&w, 4, 1, fw);
-	fwrite(&h, 4, 1, fw);
-	fwrite(end, 4, 7, fw);
 
-	for ( i = 0 ; i < h ; i++ ){
-		for ( j = 0 ; j < w ; j++ ){
-			fwrite( &rastr1[i][j] , 1 , 3 , fw );
-			fwrite(pad1, padding1, 1, fw);  
-		}
-	}
+    printf(" razmery: %d %d\n", size , size2);
+    
 
-	return 0;
+
+   for(i=0; i<h ; i++)
+    {
+	for(j=0; j<w ; j++)
+        {
+		for (z=0; z<3; z++)
+                   
+                        if (rastr1[i][j][z] != rastr2[i][j][z])	{		
+                              chet++;
+	          break;  
+                   }
+                    
+ 
+        }
+    }  
+
+    printf(" kolichestvo raznih pixelei: %d\n", chet); 
+
+	
+    return 0; 
 }
